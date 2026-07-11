e9fce25c37966f532ca2944636b1277cc86181d3
Leo <leo.bastigkeit@gmail.com>
2026-07-11 03:04:18 +0200

    Add initial implementation of SDL3 window and renderer

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vortex - Minimal SDL3 example
+
+Build with:
+```bash
+cmake --build --preset linux-release
+```
+Or:
+```bash
+cmake --build --preset windows-release
+```
+files are placed into `./bin/`
--- src/Vortex/Client/main.cpp
+++ src/Vortex/Client/main.cpp
@@ -1,6 +1,46 @@
+#include <cstdint>
 #include <print>
+
+#include <SDL3/SDL.h>
 
 int main() {
     std::println("Hello, Client!");
+
+    //------------------------------------------
+    // Super basic SDL3 window creation
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    // Window and renderer
+    std::uint32_t const flags = SDL_WINDOW_HIGH_PIXEL_DENSITY;
+
+    if (!SDL_CreateWindowAndRenderer("Vortex", 600, 400, flags, &window, &renderer)) {
+        std::println("SDL_CreateWindowAndRenderer Error: {}", SDL_GetError());
+        std::abort();
+    }
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
+    // Run
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_EVENT_QUIT) {
+            running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // RGB values (black)
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        
+        SDL_Delay(10);
+    }
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
